8eb06a3fb3081640e8d534073c3f93eaeec1def1
amirhanna2001 <amirhanna111200@gmail.com>
2023-07-07 16:58:12 +0300

    Add project files.

---
+++ E-CommerceWebApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_CommerceWebApplication.Data;
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
