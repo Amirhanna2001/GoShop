886a3db0b8bf5496341aea48a33c625b72f8205d
amirhanna2001 <amirhanna111200@gmail.com>
2023-07-18 11:57:42 +0300

    AddToCartServicesStructure

---
+++ E-CommerceWebApplication/Controllers/CartController.cs
@@ -0,0 +1,23 @@
+﻿using E_CommerceWebApplication.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_CommerceWebApplication.Controllers;
+public class CartController : Controller
+{
+    private readonly IServicesType<Product> _productServices;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public CartController( IServicesType<Product> productServices)
+    {
+        _productServices = productServices;
+    }
+    [Authorize]
+    public IActionResult AddToCart(int id)
+    {
+        var user = HttpContext.User;
+        string userId = _userManager.GetUserId(user);
+        return Ok();
+    }
+}
---
+++ E-CommerceWebApplication/Services/CartServices.cs
@@ -0,0 +1,36 @@
+﻿using E_CommerceWebApplication.Models;
+
+namespace E_CommerceWebApplication.Services;
+
+public class CartServices : IServicesType<ShoppingCart>
+{
+    public Task<ShoppingCart> Create(ShoppingCart entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ShoppingCart Delete(ShoppingCart entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ShoppingCart> Get(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<ShoppingCart>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<ShoppingCart>> Search(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ShoppingCart Update(ShoppingCart entity)
+    {
+        throw new NotImplementedException();
+    }
+}
